9cf9ef021efc72e79dafa64a488a71e725a450df
shahirsizan <shahir.sizan18@gmail.com>
2025-10-04 13:50:02 +0600

    shadcn carousel incorporated to show projects images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,24 +1,93 @@
 import { useState } from "react";
 import { projects } from "../constants";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+	Card,
+	CardAction,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+
+import khabar1 from "../assets/work_logo/khabarbari/1.png";
+import khabar2 from "../assets/work_logo/khabarbari/2.png";
+import khabar3 from "../assets/work_logo/khabarbari/3.png";
+import khabar4 from "../assets/work_logo/khabarbari/4.png";
+import khabar5 from "../assets/work_logo/khabarbari/5.png";
+import khabar6 from "../assets/work_logo/khabarbari/6.png";
+import khabar7 from "../assets/work_logo/khabarbari/7.png";
+import khabar8 from "../assets/work_logo/khabarbari/8.png";
+
+import bg1 from "../assets/work_logo/bgremover/1.png";
+import bg2 from "../assets/work_logo/bgremover/2.png";
+import bg3 from "../assets/work_logo/bgremover/3.png";
+import bg4 from "../assets/work_logo/bgremover/4.png";
+import bg5 from "../assets/work_logo/bgremover/5.png";
+import bg6 from "../assets/work_logo/bgremover/6.png";
+
+import w1 from "../assets/work_logo/weatherapp/1.png";
+import w2 from "../assets/work_logo/weatherapp/2.png";
+import w3 from "../assets/work_logo/weatherapp/3.png";
+import { DialogOverlay } from "@radix-ui/react-dialog";
+
+const imagesArray = [
+	{
+		name: "khabarbari",
+		imagesList: [
+			khabar1,
+			khabar2,
+			khabar3,
+			khabar4,
+			khabar5,
+			khabar6,
+			khabar7,
+			khabar8,
+		],
+	},
+	{
+		name: "bgremover",
+		imagesList: [bg1, bg2, bg3, bg4, bg5, bg6],
+	},
+	{
+		name: "weatherapp",
+		imagesList: [w1, w2, w3],
+	},
+];
 
 const Project = () => {
-	const [selectedProject, setSelectedProject] = useState(null);
-
-	const handleOpenModal = (project) => {
-		setSelectedProject(project);
-	};
-
-	const handleCloseModal = () => {
-		setSelectedProject(null);
-	};
+	// const [selectedProject, setSelectedProject] = useState(null);
+	// const handleOpenModal = (project) => {
+	// 	setSelectedProject(project);
+	// };
+	// const handleCloseModal = () => {
+	// 	setSelectedProject(null);
+	// };
 
 	return (
 		<section
 			id="work"
-			className="pt-16 md:pt-24 pb-24 px-[5vw] md:px-[8vw] lg:px-[12vw] relative"
+			className="SECTION pt-16 md:pt-24 pb-24 px-[5vw] md:px-[8vw] lg:px-[12vw] relative"
 		>
 			{/* TITLE */}
-			<div className="text-center mb-16">
+			<div className="TITLE text-center mb-16">
 				<h2 className="text-3xl sm:text-4xl font-bold text-white">
 					PROJECTS
 				</h2>
@@ -28,34 +97,74 @@ const Project = () => {
 				</p>
 			</div>
 
-			{/* CARDS CONTAINER */}
-			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+			{/* ALL CARDS CONTAINER */}
+			<div className="ALL-CARDS-CONTAINER grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
 				{projects.map((project) => (
-					// CARD
+					// EACH CARD
 					<div
 						key={project.id}
-						onClick={() => handleOpenModal(project)}
-						className="border border-gray-700 bg-gray-900/70 rounded-2xl shadow-2xl hover:shadow-purple-500/30"
+						// onClick={() => handleOpenModal(project)}
+						className="EACH-CARD flex flex-col border border-gray-700 bg-gray-900/70 rounded-xl shadow-2xl hover:shadow-purple-500/20 p-2"
 					>
 						{/* IMAGE */}
-						<div className="p-4 rounded-xl">
-							<img
-								src={project.image}
-								className="w-full h-48 object-cover rounded-xl overflow-hidden"
-							/>
-						</div>
+						<Dialog>
+							<DialogTrigger asChild>
+								{/* IMAGE */}
+								<div className="p-2 md:p-4 rounded-xl">
+									<img
+										src={project.image}
+										className="w-full h-48 object-cover rounded-xl overflow-hidden"
+									/>
+								</div>
+							</DialogTrigger>
+
+							<DialogContent
+								className="w-[90vw] md:w-[65vw] p-0 rounded-xl"
+								customClassName="bg-black/80"
+								showCloseButton={false}
+							>
+								<Carousel opts={{ loop: true }}>
+									<CarouselContent className="">
+										{imagesArray
+											.find((item) => {
+												return item.name === project.id;
+											})
+											.imagesList.map(
+												(imagePath, idx) => (
+													<CarouselItem
+														key={idx}
+														className=""
+													>
+														<div className="w-full rounded-xl">
+															<img
+																className="w-full h-auto object-contain rounded-xl overflow-hidden"
+																src={imagePath}
+															/>
+														</div>
+													</CarouselItem>
+												)
+											)}
+									</CarouselContent>
+									<CarouselPrevious />
+									<CarouselNext />
+								</Carousel>
+							</DialogContent>
+						</Dialog>
 
 						{/* TEXTUAL DATA */}
-						<div className="p-6">
-							<h3 className="text-2xl font-bold text-white mb-2">
+						<div className="TEXT-DATA p-2 md:p-6">
+							{/* TITLE */}
+							<h3 className="text-lg md:text-2xl font-bold text-white md:mb-2">
 								{project.title}
 							</h3>
 
-							<p className="text-gray-500 mb-4 pt-4">
+							{/* DESC */}
+							<p className="text-sm text-gray-500 mb-2 md:mb-4 pt-2 md:pt-4">
 								{project.description}
 							</p>
 
-							<div className="mb-4">
+							{/* TECHS LIST */}
+							<div className="mb-2 md:mb-4">
 								{project.tags.map((tag, index) => (
 									<span
 										key={index}
@@ -66,95 +175,34 @@ const Project = () => {
 								))}
 							</div>
 
-							<div className="flex gap-4">
-								<a
-									href={project.githubLink}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
-								>
-									Github
-								</a>
+							<div className="flex flex-1"></div>
+						</div>
 
-								<a
-									href={project.liveLink}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
-								>
-									Live
-								</a>
-							</div>
+						<div className="flex flex-1"></div>
+
+						{/* GITHUB & LIVE BUTTON */}
+						<div className="flex gap-4 px-4 pb-4">
+							<a
+								href={project.githubLink}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-md text-sm lg:text-lg font-semibold text-center"
+							>
+								Github
+							</a>
+
+							<a
+								href={project.liveLink}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-md text-sm lg:text-lg font-semibold text-center"
+							>
+								Live
+							</a>
 						</div>
 					</div>
 				))}
 			</div>
-
-			{/* MODAL CONTAINER */}
-			{/* {selectedProject && (
-				<div className="MODAL fixed top-0 right-0 bottom-0 left-0 z-50 flex items-center justify-center bg-black/80">
-					
-					<div className="bg-gray-900 rounded-xl shadow-2xl w-[90%] max-w-3xl max-h-7/12 relative">
-						<div className="flex justify-end p-4">
-							<button
-								onClick={() => {
-									handleCloseModal();
-								}}
-								className="text-white text-4xl font-bold hover:text-purple-500 cursor-pointer"
-							>
-								&times;
-							</button>
-						</div>
-
-						<div className="flex flex-col">
-							<div className="w-full flex justify-center bg-gray-900 px-3 ">
-								<img
-									src={selectedProject.image}
-									className="w-full object-contain rounded-2xl overflow-hidden shadow-2xl"
-								/>
-							</div>
-
-							<div className="lg:p-8 p-6">								<h3 className="text-2xl lg:text-3xl font-bold text-white mb-4 text-md">
-									{selectedProject.title}
-								</h3>
-								<p className="text-gray-400 mb-6 lg:text-base text-xs">
-									{selectedProject.description}
-								</p>
-								<div className="mb-6">
-									{selectedProject.tags.map((tag, index) => (
-										<span
-											key={index}
-											className="inline-block bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full p-1.5 mr-1.5 mb-2"
-										>
-											{tag}
-										</span>
-									))}
-								</div>
-
-								<div className="flex gap-4">
-									<a
-										href={selectedProject.githubLink}
-										target="_blank"
-										rel="noopener noreferrer"
-										className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
-									>
-										View Code
-									</a>
-
-									<a
-										href={selectedProject.liveLink}
-										target="_blank"
-										rel="noopener noreferrer"
-										className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
-									>
-										View Live
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			)} */}
 		</section>
 	);
 };
